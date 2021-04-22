23407f8876c5a74a1b2dccdaa07194be3c60fd5e
Henri <ecch9210@gmail.com>
2021-04-22 13:50:22 -0500

    Dashboard html draft

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// Variable identification
+
+let savedcities = [];
+let townFormEl = $("#search-form");
+let townInputEl = $("#search-input");
+let wContainerEl = $("#selected-weather-box");
+let townCurrentInputEl = $("#current-city")
