9433a915632fd45ebe26b710676aa557376b6f1c
Henri <ecch9210@gmail.com>
2021-04-22 19:53:46 -0500

    Weather javaScript Draft2

--- assets/script.js
+++ assets/script.js
@@ -12,74 +12,78 @@ let pSearchEl = $("#p-search-b");
 //Fetch API
 // API key : a9e17a63f8c99240e7dabe900a8ce415
 let goTownWeather = function (town) {
-    console.log("goTownWeather")
-fetch ('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${a9e17a63f8c99240e7dabe900a8ce415}')
-.then (function (response){
-    response.json()
-})
-.then(function(data){
-        dWeather (data, town);
+  console.log("goTownWeather");
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${town}&units=imperial&appid=a9e17a63f8c99240e7dabe900a8ce415`
+  )
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      dWeather(data, town);
     });
-
 };
 
-let sSearch = function (){
-    localStorage.setitem("savedCities", JSON.stringify(savedCities));
-}
+let sSearch = function () {
+  localStorage.setitem("savedCities", JSON.stringify(savedCities));
+};
 
 //Clean older content
-let dWeather = function (weather, sCity){
-    wContainerEl.textContent="";
-    townSearchInputEl.textContent=sCity;
+let dWeather = function (weather, sCity) {
+  wContainerEl.textContent = "";
+  townSearchInputEl.textContent = sCity;
 
-    console.log (weather);
-    console.log (sCity);
+  console.log(weather);
+  console.log(sCity);
 
-// Date
-let presentDate = $("<span/>")
-presentDate.textContent= " (" + moment(weather.dt.value).format("MMM D, YYYY") + ")";
-townSearchInputEl.appendChild(presentDate);
+  // Date
+  let presentDate = $("<span/>");
+  presentDate.textContent =
+    " (" + moment(weather.dt.value).format("MMM D, YYYY") + ")";
+  townSearchInputEl.append(presentDate);
 
+  //Weather Icon
 
-//Weather Icon
+  let weatherIcon = $("<img/>");
+  weatherIcon.attr("src", "");
 
-let weatherIcon = $("<img/>");
-weatherIcon.setAttr(src = "")
+  // Temperature
+  let tempEl = $("<span/>");
+  tempEl.text ("Temperature: " + weather.main.temp + " ºF");
+  tempEl.addClass ("data-list");
 
-// Temperature
-let tempEl = $("<span/>")
-tempEl.textContent = "Temperature: " + weather.main.temp + " ºF";
-tempEl.classList = "data-list";
+  //Humidity
+  let humEl = $("<span/>");
+  tempEl.text ("Humidity: " + weather.main.humidity + " %");
+  tempEl.addClass ("data-list");
 
-//Humidity
-let humEl = $("<span/>")
-tempEl.textContent = "Humidity: " + weather.main.humidity + " %";
-tempEl.classList = "data-list";
+  //Wind
+  let windEl = $("<span/>");
+  tempEl.text ("Wind speed: " + weather.wind.speed + " MPH");
+  tempEl.addClass ("data-list");
 
-//Wind
-let windEl = $("<span/>")
-tempEl.textContent = "Wind speed: " + weather.wind.speed + " MPH";
-tempEl.classList = "data-list";
-
-//Appending to box
-wContainerEl.appendChild (tempEl);
-wContainerEl.appendChild (humEl);
-wContainerEl.appendChild (windEl);
-
-
-}
+  //Appending to box
+  wContainerEl.append(tempEl);
+  wContainerEl.append(humEl);
+  wContainerEl.append(windEl);
+};
 // Search button handler
 // Trim: removes whitespace form both ends of a string
 
 let searchHandler = function (event) {
-    event.preventDefault();
-    let town = townFormEl.value.trim();
-    if (town){
+  console.log("submit");
+  event.preventDefault();
 
-    };
+  let town = townTitleEl.val().trim();
+  console.log(town);
+  if (town) {
+    goTownWeather(town);
+  }
 };
 
-
+townFormEl.on("submit", searchHandler);
 
 // var formSumbitHandler = function(event){
 //     event.preventDefault();
