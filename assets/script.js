2cbf87e2b5de87b5768c94326bd6dbd7ca7d7b51
Henri <ecch9210@gmail.com>
2021-04-23 18:02:33 -0500

    Weather Dashboard First row working

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,37 @@
 // Variable identification
 
 let savedCities = [];
-let townFormEl = $("#search-form");
-let townTitleEl = $("#search-input");
-let wContainerEl = $("#selected-weather-box");
-let townSearchInputEl = $("#current-city");
-let especulationTitle = $("#especulation");
-let fiveDayBox = $("#5-box");
-let pSearchEl = $("#p-search-b");
+let townFormEl = document.querySelector("#search-form");
+let townTitleEl = document.querySelector("#search-input");
+let wContainerEl = document.querySelector("#selected-weather-box");
+let townSearchInputEl = document.querySelector("#current-city");
+let especulationTitle = document.querySelector("#especulation");
+let fiveDayBox = document.querySelector("#five-box");
+let pSearchEl = document.querySelector("#p-search-b");
+
+// Search button handler
+// Trim: removes whitespace form both ends of a string
+
+let searchHandler = function (event) {
+    console.log("click");
+    event.preventDefault();
+  
+    let town = townTitleEl.value.trim();
+    console.log(town);
+    if (town) {
+      goTownWeather(town);
+    //    go5Days(town);
+    //    savedCities.unshift({town});
+    //   townTitleEl.value = "";
+    // } else {
+    //     alert ("Enter a City");
+    // }
+    // sSearch();
+    // pSearch();
+  };
+}
+  
+  townFormEl.addEventListener("click", searchHandler);
 
 //Fetch API
 // API key : a9e17a63f8c99240e7dabe900a8ce415
@@ -39,51 +63,36 @@ let dWeather = function (weather, sCity) {
   console.log(sCity);
 
   // Date
-  let presentDate = $("<span/>");
-  presentDate.textContent =
-    " (" + moment(weather.dt.value).format("MMM D, YYYY") + ")";
-  townSearchInputEl.append(presentDate);
+  let presentDate = document.createElement("span");
+  presentDate.textContent = (
+    " (" + moment(weather.dt.value).format("MMM D, YYYY") + ")");
+  townSearchInputEl.appendChild (presentDate);
 
   //Weather Icon
 
-  let weatherIcon = $("<img/>");
-  weatherIcon.attr("src", "");
-
+  let weatherIcon = document.createElement("img");
+  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    townSearchInputEl.appendChild (weatherIcon);
   // Temperature
-  let tempEl = $("<span/>");
-  tempEl.text ("Temperature: " + weather.main.temp + " ºF");
-  tempEl.addClass ("data-list");
+  let tempEl = document.createElement("span");
+  tempEl.textContent= "Temperature: " + weather.main.temp + " ºF";
+  tempEl.classList = "data-list";
 
   //Humidity
-  let humEl = $("<span/>");
-  tempEl.text ("Humidity: " + weather.main.humidity + " %");
-  tempEl.addClass ("data-list");
+  let humEl = document.createElement("span");
+  tempEl.textContent = "Humidity: " + weather.main.humidity + " %";
+  tempEl.classList = "data-list";
 
   //Wind
-  let windEl = $("<span/>");
-  tempEl.text ("Wind speed: " + weather.wind.speed + " MPH");
-  tempEl.addClass ("data-list");
+  let windEl = document.createElement("span");
+  tempEl.textContent = "Wind speed: " + weather.wind.speed + " MPH";
+  tempEl.classList = "data-list";
 
   //Appending to box
-  wContainerEl.append(tempEl);
-  wContainerEl.append(humEl);
-  wContainerEl.append(windEl);
+  wContainerEl.appendChild(tempEl);
+  wContainerEl.appendChild(humEl);
+  wContainerEl.appendChild(windEl);
 };
-// Search button handler
-// Trim: removes whitespace form both ends of a string
-
-let searchHandler = function (event) {
-  console.log("submit");
-  event.preventDefault();
-
-  let town = townTitleEl.val().trim();
-  console.log(town);
-  if (town) {
-    goTownWeather(town);
-  }
-};
-
-townFormEl.on("submit", searchHandler);
 
 // var formSumbitHandler = function(event){
 //     event.preventDefault();
